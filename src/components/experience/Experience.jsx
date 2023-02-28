97100a33d7249846c7b54132629318fbc92aeed0
nicopina <pinanicolasagustin@gmail.com>
2023-02-28 15:35:45 -0300

    exp

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -3,7 +3,16 @@ import './experience.css'
 const Experience = () => {
   return (
     <section id='experience'>
+      <h4>The Skilss I Have</h4>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
 
+        </div>
+        <div className="experience__backend">
+          
+        </div>
+      </div>
     </section>
   )
 }
