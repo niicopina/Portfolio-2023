87ae20092c248420ec88cb005d5e3e791e567b9b
nicopina <pinanicolasagustin@gmail.com>
2023-02-28 03:54:40 -0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+import Header from './components/header/Header'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Nav from './components/nav/Nav'
+import Footer from './components/footer/Footer'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Experience from './components/experience/Experience'
+
+
+
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+        <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <Services />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
+        <Footer />
+        
+        
+
+
+
+    </>
+  )
+}
+
+export default App
