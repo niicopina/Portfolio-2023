32364d1cf3df69362cddb9d00fc69b2b488ff3e4
nicopina <pinanicolasagustin@gmail.com>
2023-02-28 15:07:28 -0300

    jsx about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
     <section id='about'>
+      <h5>Get To know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="about me image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ Years Working</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>20+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur a
+            Reiciendis odit perspiciatis iure!
+            Sunt, modi ducimus maxime itaque et adipisci 
+            Quas voluptates maxime possimus nam excepturi 
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
 
     </section>
   )
