9d3a09a0482fde2ebd30ce43f356db0ae606b2f5
nicopina <pinanicolasagustin@gmail.com>
2023-02-28 20:39:18 -0300

    contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,25 +12,31 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className='contact__option'>
-            <MdOutlineEmail />
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>pinanicolasagustin@gmail.com</h5>
-            <a href="mailto:pinanicolasagustin@gmail.com">Send a message</a>
+            <a href="mailto:pinanicolasagustin@gmail.com" target="_blank">Send a message</a>
 
           </article>
           <article className='contact__option'>
-            <BsTelephone />
+            <BsTelephone className='contact__option-icon'/>
             <h4>Phone</h4>
             <h5>54 011 63612903</h5>
-            <a href="mailto:pinanicolasagustin@gmail.com">Send a message</a>
+            <a href="https://api.whatsapp.com/send?phone=+1163612903" target="_blank">Send a message</a>
           </article>
           <article className='contact__option'>
-            <BsLinkedin />
+            <BsLinkedin className='contact__option-icon'/>
             <h4>Linkedin</h4>
             <h5>Nicolás Agustín Piña</h5>
-            <a href="https://www.linkedin.com/in/nicolas-agustin-pina/">Send a message</a>
+            <a href="https://www.linkedin.com/in/nicolas-agustin-pina/" target="_blank">Send a message</a>
           </article>
         </div>
+        <form action="">
+          <input type="text" name='name' placeholder='Your full name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
 
         <form action=""></form>
       </div>
