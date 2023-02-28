75626cf57ec2e4812753ec860c508063882fac86
nicopina <pinanicolasagustin@gmail.com>
2023-02-28 04:34:43 -0300

    socials

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import ME from '../../assets/me.jpg'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -10,6 +12,12 @@ const Header = () => {
         <h1>Nico Piña</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
       </div>
     </header>
   )
